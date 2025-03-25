2d02e75b40d6aa837595600690418eb8ef49a7d1
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-03-25 04:07:44 +0000

    _

--- _print.c
+++ _print.c
@@ -3,6 +3,39 @@
 #include <stdarg.h>
 
 /**
+ * print_char - Prints a character.
+ * @args: Argument list containing the character to print.
+ *
+ * Return: Number of characters printed.
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: Argument list containing the string to print.
+ *
+ * Return: Number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int counter = 0;
+
+	if (!str)
+		str = "(null)";
+
+	while (*str)
+		counter += write(1, str++, 1);
+
+	return (counter);
+}
+
+/**
  * _printf - Custom implementation of printf function.
  * @format: Format string containing the characters and specifiers.
  *
@@ -10,37 +43,33 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list a;
-	int i, counter;
-	char c;
+	va_list args;
+	int i, counter = 0;
 
-	counter = 0;
-	va_start(a, format);
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
 	for (i = 0; format[i]; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0' &&
-		    format[i + 1] != 'c' && format[i + 1] != '%')
-			continue;
-
-		if (format[i] == '%' && format[i + 1] == 'c')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			c = va_arg(a, int);
-			write(1, &c, 1);
-			counter++;
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			write(1, &format[i], 1);
-			counter++;
+			if (format[i + 1] == 'c')
+				counter += print_char(args);
+			else if (format[i + 1] == 's')
+				counter += print_string(args);
+			else if (format[i + 1] == '%')
+				counter += write(1, "%", 1);
+			else
+				counter += write(1, &format[i], 1);
 			i++;
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			counter++;
-		}
+			counter += write(1, &format[i], 1);
 	}
-	va_end(a);
+
+	va_end(args);
 	return (counter);
 }
+
