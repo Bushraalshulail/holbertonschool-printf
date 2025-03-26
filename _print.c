6e80353977a5a87a0b7e43c3474f7127f56187ef
Hamad <hdahash@hotmail.com>
2025-03-26 11:44:32 +0000

    Add print_int to the function printf

--- _print.c
+++ _print.c
@@ -14,6 +14,34 @@ int print_char(va_list args)
 
 	return (write(1, &c, 1));
 }
+/**
+ * print_int - Prints a integer.
+ * @args: Argument list containing the integer to print.
+ *
+ * Return: Number of characters printed.
+ */
+int print_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int i = 20, counter = 0;
+	char s[20];
+	char c = '-';
+
+	if (n < 0)
+	{
+		n *= -1;
+		counter += write(1, &c, 1);
+	}
+	while (n > 0)
+	{
+		c =  n % 10 + 48;
+		s[i] = c;
+		i--;
+		n /= 10;
+	}
+	counter += write(1, s + i + 1, 20 - i);
+	return (counter);
+}
 
 /**
  * print_string - Prints a string.
@@ -59,6 +87,8 @@ int _printf(const char *format, ...)
 				return (-1);
 			if (format[i + 1] == 'c')
 				counter += print_char(args);
+			else if (format[i + 1] == 'i' || format[i + 1] == 'd')
+				counter += print_int(args);
 			else if (format[i + 1] == 's')
 				counter += print_string(args);
 			else if (format[i + 1] == '%')
